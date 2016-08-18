10eb2f45d078f9c88731f095117a43831d29af6b
James Baxley <james.baxley@newspring.cc>
2016-08-18 14:17:13 -0400

    add a few addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,12 @@
-import { configure } from '@kadira/storybook';
+import { configure, addDecorator, setAddon } from "@kadira/storybook";
+import centered from "@kadira/react-storybook-decorator-centered";
+import infoAddon from "@kadira/react-storybook-addon-info";
+
 import '../src/index.css';
 
-function loadStories() {
-  require('../src/stories');
-}
+setAddon(infoAddon);
+// addDecorator(centered);
 
-configure(loadStories, module);
+configure(() => {
+  require('../src/stories');
+}, module);
--- src/stories/index.js
+++ src/stories/index.js
@@ -3,15 +3,20 @@ import { storiesOf, action, linkTo } from '@kadira/storybook';
 import Button from './Button';
 import Welcome from './Welcome';
 
-storiesOf('Welcome', module)
-  .add('to Storybook', () => (
-    <Welcome showApp={linkTo('Button')}/>
-  ));
+// storiesOf('Welcome', module)
+//   .add('to Storybook', () => (
+//     <Welcome showApp={linkTo('Button')}/>
+//   ));
 
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
-  ))
-  .add('with some emojies', () => (
+const story = storiesOf('Button', module)
+
+story.addWithInfo(
+  'with text', 
+  `
+    Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
+  `,
+  () => <Button onClick={action('clicked')}>Hello Button</Button>
+  , { inline: true }
+).add('with some emojies', () => (
     <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
   ));
