0eaf23c814ce767f753e0c36aa7d691abe611cee
Sam Claridge <st.claridge@gmail.com>
2016-08-21 10:30:37 -0400

    add buttons

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,30 @@ const story = storiesOf("Button", module);
 
 // story.add("with textings", () => <h1 class="text-primary">Hai, I'm Junction</h1>);
 story.addWithInfo(
-  "with text",
+  "Primary",
   `
     Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
   `,
-  () => <h1>Hai, I'm Junction</h1>
+  () => <a href="#" className="btn h5">You Won't Click Me</a>
+);
+story.addWithInfo(
+  "Filled",
+  `
+    Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
+  `,
+  () => <a href="#" className="btn--filled h5">You Won't Click Me</a>
+);
+story.addWithInfo(
+  "Small",
+  `
+    Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
+  `,
+  () => <a href="#" className="btn--small h6">You Won't Click Me</a>
+);
+story.addWithInfo(
+  "Grey",
+  `
+    Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
+  `,
+  () => <a href="#" className="btn--dark-tertiary h5">You Won't Click Me</a>
 );
