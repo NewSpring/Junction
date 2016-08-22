e407f030f60685aca85e7eb3562ba9effceddd3d
Sam Claridge <st.claridge@gmail.com>
2016-08-22 14:43:33 -0400

    fix the linting error properly

--- src/elements/checkboxes/index.story.tsx
+++ src/elements/checkboxes/index.story.tsx
@@ -10,8 +10,8 @@ story.addWithInfo(
   `
     Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
   `,
+  // tslint:disable
   () => <Checkbox className="input hard">
-    // tslint:disable-next-line
     <h6 className="soft-left push-half-left flush-bottom text-left float-left">This is technically a radio button, we need to define checkboxes</h6>
     <input type="checkbox" id="checkboxName"/>
     <label for="checkboxName"></label>
