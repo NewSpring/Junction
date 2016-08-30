e944d176e91d2da178a86e1005fbb124e480a562
Jake Dawkins <dawkinsjh@gmail.com>
2016-08-30 09:50:14 -0400

    removed jsx and jsx.map files

--- src/elements/checkboxes/index.story.jsx
+++
@@ -1,7 +0,0 @@
-"use strict";
-const storybook_1 = require("@kadira/storybook");
-const index_tsx_1 = require("./index.tsx");
-const story = storybook_1.storiesOf("Checkboxes", module);
-story.add("Basic Checkbox", () => (<index_tsx_1.default name="name-here" value="value-here"/>));
-story.add("Checked Checkbox", () => (<index_tsx_1.default name="name-here" value="value-here" checked/>));
-//# sourceMappingURL=index.story.jsx.map
