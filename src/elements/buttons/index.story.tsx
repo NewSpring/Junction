b518e04882b5f6812f6dac05328cd2b0d3cef91f
Daniel Edwards <cdaniel.edwards@gmail.com>
2016-08-31 16:59:19 -0400

    Finished out button variations

--- src/elements/buttons/index.story.tsx
+++ src/elements/buttons/index.story.tsx
@@ -4,46 +4,74 @@ import Button, {
   ButtonFilled,
   ButtonSmall,
   ButtonGray,
+  ButtonDisabled,
+  ButtonCorners,
+  ButtonLight,
+  ButtonDarkTertiary,
+  ButtonDarkPrimary,
+  ButtonDarkSecondary,
+  ButtonAlert,
+  ButtonIcon,
 } from "./index.tsx";
 
 const story = storiesOf("Buttons", module);
 
-story.addWithInfo(
+story.add(
   "Primary",
-  // tslint:disable-next-line
-  `
-    Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
-  `,
-  () => <div className="text-center one-whole"><Button>Click Here</Button></div>,
-  { propTables: false }
+  () => <div className="text-center one-whole"><Button>Click Here</Button></div>
 );
 
-story.addWithInfo(
+story.add(
   "Filled",
-  // tslint:disable-next-line
-  `
-    Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
-  `,
-  () => <div className="text-center one-whole"><ButtonFilled>Click Here</ButtonFilled></div>,
-  { propTables: false }
+  () => <div className="text-center one-whole"><ButtonFilled>Click Here</ButtonFilled></div>
 );
 
-story.addWithInfo(
+story.add(
   "Small",
-  // tslint:disable-next-line
-  `
-    Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
-  `,
-  () => <div className="text-center one-whole"><ButtonSmall>You Won't Click Me</ButtonSmall></div>,
-  { propTables: false }
+  () => <div className="text-center one-whole"><ButtonSmall>You Won't Click Me</ButtonSmall></div>
 );
 
-story.addWithInfo(
+story.add(
   "Grey",
-  // tslint:disable-next-line
-  `
-    Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
-  `,
-  () => <div className="text-center one-whole"><ButtonGray>You Won't Click Me</ButtonGray></div>,
-  { propTables: false }
+  () => <div className="text-center one-whole"><ButtonGray>You Won't Click Me</ButtonGray></div>
+);
+
+story.add(
+  "Disabled",
+  () => <div className="text-center one-whole"><ButtonDisabled>You Won't Click Me</ButtonDisabled></div>
+);
+
+story.add(
+  "Corners",
+  () => <div className="text-center one-whole"><ButtonCorners>You Won't Click Me</ButtonCorners></div>
+);
+
+story.add(
+  "Light",
+  () => <div className="text-center one-whole"><ButtonLight>You Won't Click Me</ButtonLight></div>
+);
+
+story.add(
+  "Dark Primary",
+  () => <div className="text-center one-whole"><ButtonDarkPrimary>You Won't Click Me</ButtonDarkPrimary></div>
+);
+
+story.add(
+  "Dark Secondary",
+  () => <div className="text-center one-whole"><ButtonDarkSecondary>You Won't Click Me</ButtonDarkSecondary></div>
+);
+
+story.add(
+  "Dark Tertiary",
+  () => <div className="text-center one-whole"><ButtonDarkTertiary>You Won't Click Me</ButtonDarkTertiary></div>
+);
+
+story.add(
+  "Alert",
+  () => <div className="text-center one-whole"><ButtonAlert>You Won't Click Me</ButtonAlert></div>
+);
+
+story.add(
+  "Icon",
+  () => <div className="text-center one-whole"><ButtonIcon>You Won't Click Me</ButtonIcon></div>
 );
