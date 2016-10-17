048d3cd7fb9876c5e57f66e5e65009bd539906c6
Jake Dawkins <dawkinsjh@gmail.com>
2016-10-17 16:30:58 -0400

    added cards stories

--- src/components/cards/index.story.tsx
+++ src/components/cards/index.story.tsx
@@ -1,9 +1,14 @@
-/*
 import { storiesOf } from "@kadira/storybook";
+import backgrounds from "react-storybook-addon-backgrounds";
+import centered from "../../../.storybook/decorators/centered/index.tsx";
+import defaultColors from "../../defaults.tsx";
 
 import Card from "./index.tsx";
 
-const story = storiesOf("Cards", module);
+const story = storiesOf("Cards", module)
+  .addDecorator(centered)
+  .addDecorator(backgrounds(defaultColors()))
+  ;
 
 let body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." +
   "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
@@ -17,9 +22,8 @@ story.add(
 );
 
 story.add(
-  "Card with image",
+  "Card with card__image",
   () => (
       <Card image="https://placehold.it/350x150" title={title} body={body}/>
   )
 );
-*/
