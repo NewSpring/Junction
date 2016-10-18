d3e1b7cb4aa73e15320d3bd6f3f4fe58529e3a2b
Sam Claridge <st.claridge@gmail.com>
2016-10-18 14:34:37 -0400

    add markup for ordered lists

--- src/elements/lists/index.story.tsx
+++ src/elements/lists/index.story.tsx
@@ -3,7 +3,10 @@ import backgrounds from "react-storybook-addon-backgrounds";
 import centered from "../../../.storybook/decorators/centered/index.tsx";
 import defaultColors from "../../defaults.tsx";
 
-import List from "./index.tsx";
+import {
+  UnorderedList,
+  OrderedList,
+} from "./index.tsx";
 
 const story = storiesOf("Lists", module)
   .addDecorator(centered)
@@ -13,14 +16,25 @@ const story = storiesOf("Lists", module)
 story.add(
   "Basic Unordered List",
   () => (
-    <List className="one-whole">
+    <UnorderedList className="one-whole">
       <li>List Item 1</li>
-      <List className="one-whole">
+      <UnorderedList className="one-whole">
         <li> Item A </li>
         <li> Item B </li>
-      </List>
+      </UnorderedList>
       <li>List Item 2</li>
       <li>List Item 3</li>
-    </List>
+    </UnorderedList>
+  )
+);
+
+story.add(
+  "Basic Ordered List",
+  () => (
+    <OrderedList className="one-whole">
+      <li>List Item 1</li>
+      <li>List Item 2</li>
+      <li>List Item 3</li>
+    </OrderedList>
   )
 );
