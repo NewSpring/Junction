a021faaaab9a3f9d7a0b47163da19a19832aba99
Jake Dawkins <dawkinsjh@gmail.com>
2016-10-19 10:54:05 -0400

    added decorators

--- src/components/panels/index.story.js
+++ src/components/panels/index.story.js
@@ -1,9 +1,15 @@
 import { storiesOf } from "@kadira/storybook";
+import backgrounds from "react-storybook-addon-backgrounds";
+import centered from "../../../.storybook/decorators/centered";
+import defaultColors from "../../defaults";
 
 import Panel from "./index";
 
-const story = storiesOf("Panels", module);
+const story = storiesOf("Panels", module)
+  .addDecorator(centered)
+  .addDecorator(backgrounds(defaultColors()));
 
 story.add(
-  "Basic Panel", () => <Panel />
+  "Basic Panel",
+  () => <Panel />
 );
