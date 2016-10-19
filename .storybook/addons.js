ecb96833cb0da6e0ca2d154c6a49766a3c2c65d9
Drew Delianides <github@delianides.com>
2016-10-19 11:24:54 -0400

    removed and cleanup unused storybook addons (#70)

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,14 +1,15 @@
-import "storybook-addon-specifications/register";
+/* eslint-disable */
 import 'react-storybook-addon-backgrounds/register';
-// import "./addons/apollo";
-import "./addons/stylestats";
 import "storybook-readme/register";
+// import "./addons/apollo";
+// import "./addons/stylestats";
 
-import addons from '@kadira/storybook-addons';
+import addons from "@kadira/storybook-addons";
+/* eslint-enable */
 
-addons.register("app info", api => {
-    api.setOptions({
-      name: "Junction Framework",
-      url: "newspring.cc"
-    })
+addons.register("app info", (api) => {
+  api.setOptions({
+    name: "Junction Framework",
+    url: "newspring.cc",
+  });
 });
