c97e27fdd8478f62e55944d4f6e6bdb776d86b76
edolyne <ed@edolyne.com>
2016-10-19 15:11:18 -0400

    Fixing lint error

--- src/helpers/text/index.story.js
+++ src/helpers/text/index.story.js
@@ -13,7 +13,11 @@ story.add(
     () => (
       <div className="one-half text-left">
         <h2>Heading 2</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit amet urna nisi. Nam rhoncus, sem aliquet sollicitudin lacinia, tellus lorem sodales ipsum, id sollicitudin odio orci vel risus. Maecenas dictum bibendum efficitur. Morbi tortor ex, volutpat varius pretium eu, lacinia a lectus. Donec pellentesque aliquam felis, in sagittis urna suscipit at. Cras ac quam a diam posuere congue sit amet a orci. Praesent ex lorem, hendrerit in leo pharetra, tempus euismod lorem. Mauris sed iaculis tortor. Donec purus nulla, ultrices vel justo quis, congue sagittis leo. Vestibulum ut lorem dapibus, fermentum nisi at, volutpat libero. Vivamus sed nunc tellus.</p>
+        <p>
+          lorem ipsum dolor sit amet, consectetur adipiscing elit. etiam sit amet urna nisi.
+          nam rhoncus, sem aliquet sollicitudin lacinia, tellus lorem sodales ipsum,
+          id sollicitudin odio orci vel risus.
+        </p>
       </div>
     )
 );
@@ -23,7 +27,11 @@ story.add(
     () => (
       <div className="one-half text-center">
         <h2>Heading 2</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit amet urna nisi. Nam rhoncus, sem aliquet sollicitudin lacinia, tellus lorem sodales ipsum, id sollicitudin odio orci vel risus. Maecenas dictum bibendum efficitur. Morbi tortor ex, volutpat varius pretium eu, lacinia a lectus. Donec pellentesque aliquam felis, in sagittis urna suscipit at. Cras ac quam a diam posuere congue sit amet a orci. Praesent ex lorem, hendrerit in leo pharetra, tempus euismod lorem. Mauris sed iaculis tortor. Donec purus nulla, ultrices vel justo quis, congue sagittis leo. Vestibulum ut lorem dapibus, fermentum nisi at, volutpat libero. Vivamus sed nunc tellus.</p>
+        <p>
+          Loem ipsum dolor sit amet, consectetur adipiscing elit. etiam sit amet urna nisi.
+          nam rhoncus, sem aliquet sollicitudin lacinia, tellus lorem sodales ipsum,
+          id sollicitudin odio orci vel risus.
+        </p>
       </div>
     )
 );
@@ -33,7 +41,11 @@ story.add(
     () => (
       <div className="one-half text-right">
         <h2>Heading 2</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit amet urna nisi. Nam rhoncus, sem aliquet sollicitudin lacinia, tellus lorem sodales ipsum, id sollicitudin odio orci vel risus. Maecenas dictum bibendum efficitur. Morbi tortor ex, volutpat varius pretium eu, lacinia a lectus. Donec pellentesque aliquam felis, in sagittis urna suscipit at. Cras ac quam a diam posuere congue sit amet a orci. Praesent ex lorem, hendrerit in leo pharetra, tempus euismod lorem. Mauris sed iaculis tortor. Donec purus nulla, ultrices vel justo quis, congue sagittis leo. Vestibulum ut lorem dapibus, fermentum nisi at, volutpat libero. Vivamus sed nunc tellus.</p>
+        <p>
+          lorem ipsum dolor sit amet, consectetur adipiscing elit. etiam sit amet urna nisi.
+          nam rhoncus, sem aliquet sollicitudin lacinia, tellus lorem sodales ipsum,
+          id sollicitudin odio orci vel risus.
+        </p>
       </div>
     )
 );
@@ -43,7 +55,11 @@ story.add(
     () => (
       <div className="one-half text-brand">
         <h2>Heading 2</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit amet urna nisi. Nam rhoncus, sem aliquet sollicitudin lacinia, tellus lorem sodales ipsum, id sollicitudin odio orci vel risus. Maecenas dictum bibendum efficitur. Morbi tortor ex, volutpat varius pretium eu, lacinia a lectus. Donec pellentesque aliquam felis, in sagittis urna suscipit at. Cras ac quam a diam posuere congue sit amet a orci. Praesent ex lorem, hendrerit in leo pharetra, tempus euismod lorem. Mauris sed iaculis tortor. Donec purus nulla, ultrices vel justo quis, congue sagittis leo. Vestibulum ut lorem dapibus, fermentum nisi at, volutpat libero. Vivamus sed nunc tellus.</p>
+        <p>
+          lorem ipsum dolor sit amet, consectetur adipiscing elit. etiam sit amet urna nisi.
+          nam rhoncus, sem aliquet sollicitudin lacinia, tellus lorem sodales ipsum,
+          id sollicitudin odio orci vel risus.
+        </p>
       </div>
     )
 );
