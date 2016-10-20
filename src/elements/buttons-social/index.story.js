f0c53b85b937f5576624827df3cd338994129285
Jake Dawkins <dawkinsjh@gmail.com>
2016-10-20 13:46:05 -0400

    added readme to story

--- src/elements/buttons-social/index.story.js
+++ src/elements/buttons-social/index.story.js
@@ -1,9 +1,11 @@
 import { storiesOf } from "@kadira/storybook";
 import backgrounds from "react-storybook-addon-backgrounds";
+import withReadme from "storybook-readme/with-readme";
 import centered from "../../../.storybook/decorators/centered";
 import defaultColors from "../../defaults";
 
 import ButtonSocial from "./index";
+import Readme from "./README.md";
 
 const story = storiesOf("Social Buttons", module)
   .addDecorator(centered)
@@ -11,64 +13,56 @@ const story = storiesOf("Social Buttons", module)
 
 story.addWithInfo(
   "Facebook",
-  // tslint:disable-next-line
-  `
-    Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
-  `,
-  () => (
-    <div className="text-center one-whole">
-      <ButtonSocial className="btn--facebook">
-        Facebook
-      </ButtonSocial>
-    </div>
+  withReadme(Readme,
+    () => (
+      <div className="text-center one-whole">
+        <ButtonSocial className="btn--facebook">
+          Facebook
+        </ButtonSocial>
+      </div>
+    )
   ),
   { propTables: false }
 );
 
 story.addWithInfo(
   "Google",
-  // tslint:disable-next-line
-  `
-    Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
-  `,
-  () => (
-    <div className="text-center one-whole">
-      <ButtonSocial className="btn--google">
-        Google
-      </ButtonSocial>
-    </div>
+  withReadme(Readme,
+    () => (
+      <div className="text-center one-whole">
+        <ButtonSocial className="btn--google">
+          Google
+        </ButtonSocial>
+      </div>
+    )
   ),
   { propTables: false }
 );
 
 story.addWithInfo(
   "Twitter",
-  // tslint:disable-next-line
-  `
-    Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
-  `,
-  () => (
-    <div className="text-center one-whole">
-      <ButtonSocial className="btn--twitter">
-        Twitter
-      </ButtonSocial>
-    </div>
+  withReadme(Readme,
+    () => (
+      <div className="text-center one-whole">
+        <ButtonSocial className="btn--twitter">
+          Twitter
+        </ButtonSocial>
+      </div>
+    )
   ),
   { propTables: false }
 );
 
 story.addWithInfo(
   "Instagram",
-  // tslint:disable-next-line
-  `
-    Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
-  `,
-  () => (
-    <div className="text-center one-whole">
-      <ButtonSocial className="btn--instagram">
-        Instagram
-      </ButtonSocial>
-    </div>
+  withReadme(Readme,
+    () => (
+      <div className="text-center one-whole">
+        <ButtonSocial className="btn--instagram">
+          Instagram
+        </ButtonSocial>
+      </div>
+    )
   ),
   { propTables: false }
 );
