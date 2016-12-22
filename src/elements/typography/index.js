bc06ef545e067e73de94cc8a9d6ac2e348f2d782
Jonathan Wilson Bruce <jwbmw3@gmail.com>
2016-12-22 16:07:57 -0500

    Added Typography Story

---
+++ src/elements/typography/index.js
@@ -0,0 +1,16 @@
+import { PropTypes } from "react";
+
+const Typography = () => (
+  <div>
+    <h1>H1</h1>
+    <h2>H2</h2>
+    <h3>H3</h3>
+    <h4>H4</h4>
+    <h5>H5</h5>
+    <h6>H6</h6>
+    <h7>H7</h7>
+    <p>P</p>
+  </div>
+  );
+
+export default Typography;
